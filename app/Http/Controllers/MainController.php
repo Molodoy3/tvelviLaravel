a32a2473d8d904d3ff632d5d0af10340bc00aebe
Molodoy3 <artem.nikono.2005@bk.ru>
2024-10-14 23:27:33 +0500

    a

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,7 @@ use App\Models\Article;
 use App\Models\Review;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Spatie\Sitemap\SitemapGenerator;
 
 class MainController extends Controller
 {
@@ -16,6 +17,9 @@ class MainController extends Controller
         ]);
     }*/
     public function about() {
+        //Sitema
+        dd(SitemapGenerator::create('https://tvelvi.com')->getSitemap()->writeToDisk('public', 'sitemap.xml', true));
+
         return Inertia::render('Main/About', [
             'lastReviews' => Review::query()->orderByDesc('created_at')->take(4)->get()
         ]);
