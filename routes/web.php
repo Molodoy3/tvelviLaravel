4fda901afee0c0b3a311e5a5c7303bab4f61369a
Molodoy3 <artem.nikono.2005@bk.ru>
2024-10-16 13:14:35 +0500

    a

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', [MainController::class, 'test'])->name('test');
-Route::get('/get_last_new', [MainController::class, 'getLastNew'])->name('getLastNew');
+Route::post('/get_last_new', [MainController::class, 'getLastNew'])->name('getLastNew');
 //основные страницы
 Route::get('/', [ServiceController::class, 'show'])->name('index');
 Route::get('/about', [MainController::class, 'about'])->name('about');
